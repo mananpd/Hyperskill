e4180aff964eb2da921e193ba57f94b02e6d88f4
mananpd <mpdamani1@gmail.com>
2022-04-15 14:49:59 -0700

    Finished

--- Easy Rider Bus Company/Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime
+import itertools
 
 input_json = json.loads(input())
 
@@ -9,16 +9,35 @@ def incorrect_times(bus_lines):
     for bus in bus_lines:
         bus_set.add(bus["bus_id"])
 
-    start_set = ()
-    finish_set = ()
+    start_set = set()
+    finish_set = set()
     bus_stop_dic = {}
+    on_demand_set = set()
     for bus_id in bus_set:
+        bus_stop_dic[bus_id] = set()
         for bus_line in bus_lines:
-            if
+            if bus_line["stop_type"] == "S":
+                start_set.add(bus_line["stop_name"])
+            if bus_line["stop_type"] == "F":
+                finish_set.add(bus_line["stop_name"])
+            if bus_line["stop_type"] == "O":
+                on_demand_set.add(bus_line["stop_name"])
+            if bus_line["bus_id"] == bus_id:
+                bus_stop_dic[bus_id].add(bus_line["stop_name"])
 
-    if len(wrong_time_dic) > 0:
-        for bus_id, stop_name in wrong_time_dic.items():
-            print(f'bus_id line {bus_id}: wrong time on station {stop_name}')
+    transfer_set = set()
+    for bus1, bus2 in itertools.combinations(bus_stop_dic, 2):
+        transfer = set.intersection(bus_stop_dic[bus1], bus_stop_dic[bus2])
+        transfer_set = transfer_set.union(transfer)
+
+    wrong_stop_type = set()
+    wrong_stop_type = wrong_stop_type.union(set.intersection(on_demand_set, start_set))
+    wrong_stop_type = wrong_stop_type.union(set.intersection(on_demand_set, finish_set))
+    wrong_stop_type = wrong_stop_type.union(set.intersection(on_demand_set, transfer_set))
+
+    print('On demand stops test:')
+    if len(wrong_stop_type) > 0:
+        print(f'Wrong stop type: {sorted(list(wrong_stop_type))}')
     else:
         print("OK")
 
